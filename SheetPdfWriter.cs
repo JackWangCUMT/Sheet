49ec997595e102327fe51b6b08eea5491ae13535
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2014-04-27 22:00:19 +0200

    Changed pdf export library

--- SheetPdfWriter.cs
+++ SheetPdfWriter.cs
@@ -5,21 +5,16 @@ using System.Text;
 
 namespace Sheet
 {
-    using iTextSharp.text;
-    using iTextSharp.text.pdf;
-    using System.IO;
+    using PdfSharp;
+    using PdfSharp.Drawing;
+    using PdfSharp.Pdf;
 
     public class SheetPdfWriter
     {
         #region Fields
 
-        private Func<double, float> X;
-        private Func<double, float> Y;
-        private Func<double, float> DX;
-        private Func<double, float> DY;
-        private string ttf;
-        private BaseFont bf;
-        private PdfContentByte canvas; 
+        private Func<double, double> X;
+        private Func<double, double> Y;
 
         #endregion
 
@@ -27,162 +22,133 @@ namespace Sheet
 
         public void Create(string fileName, BlockItem block)
         {
-            Document doc = CreateDocument();
+            PdfDocument document = new PdfDocument();
+            PdfPage page = document.AddPage();
+            page.Size = PageSize.A4;
+            page.Orientation = PageOrientation.Landscape;
+            XGraphics gfx = XGraphics.FromPdfPage(page);
 
-            using (var fs = new FileStream(fileName, FileMode.Create))
-            {
-                var writer = PdfWriter.GetInstance(doc, fs);
+            double width = 1260;
+            double height = 891;
+            double scaleX = page.Width.Value / width;
+            double scaleY = page.Height.Value / height;
 
-                doc.Open();
+            X = (x) => x * scaleX;
+            Y = (y) => y * scaleY;
 
-                canvas = writer.DirectContent;
+            Export(gfx, block);
 
-                Export(block);
-
-                doc.Close();
-            }
-
-            ttf = null;
-            bf = null;
-            canvas = null;
-        }
-
-        private string GetFontPath(string font)
-        {
-            DirectoryInfo windows = Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.System));
-            string fonts = Path.Combine(windows.FullName, "Fonts");
-            return Path.Combine(fonts, font);
-        }
-
-        private Document CreateDocument()
-        {
-            ttf = GetFontPath("calibri.ttf");
-            bf = BaseFont.CreateFont(ttf, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-
-            var doc = new Document(PageSize.A4.Rotate(), 0, 0, 0, 0);
-
-            float width = 1260;
-            float height = 891;
-            float scaleX = doc.PageSize.Width / width;
-            float scaleY = doc.PageSize.Height / height;
-
-            X = (x) => (float)(x * scaleX);
-            Y = (y) => (float)((height - y) * scaleY);
-            DX = (d) => (float)(d * scaleX);
-            DY = (d) => (float)(d * scaleY);
-
-            return doc;
+            document.Save(fileName);
         }
 
         #endregion
 
         #region Add
 
-        private void AddLine(double x1, double y1, double x2, double y2)
+        private void AddLine(XGraphics gfx, double x1, double y1, double x2, double y2)
         {
-            canvas.MoveTo(X(x1), Y(y1));
-            canvas.LineTo(X(x2), Y(y2));
-            canvas.Stroke();
+            gfx.DrawLine(XPens.Black, X(x1), Y(y1), X(x2), Y(y2));
         }
 
-        private void AddEllipse(double x, double y, double width, double height, bool isFilled)
+        private void AddEllipse(XGraphics gfx, double x, double y, double width, double height, bool isFilled)
         {
-            canvas.Ellipse(X(x), Y(y), X(width + x), Y(height + y));
             if (isFilled)
             {
-                canvas.Fill();
+                gfx.DrawEllipse(XPens.Black, XBrushes.Black, X(x), Y(y), X(width), Y(height));
             }
-            canvas.Stroke();
+            else
+            {
+                gfx.DrawEllipse(XPens.Black, X(x), Y(y), X(width), Y(height));
+            }
         }
 
-        private void AddRectangle(double x, double y, double width, double height, bool isFilled)
+        private void AddRectangle(XGraphics gfx, double x, double y, double width, double height, bool isFilled)
         {
-            canvas.Rectangle(X(x), Y(y + height), DX(width), DY(height));
             if (isFilled)
             {
-                canvas.Fill();
+                gfx.DrawRectangle(XPens.Black, XBrushes.Black, X(x), Y(y), X(width), Y(height));
             }
-            canvas.Stroke();
+            else
+            {
+                gfx.DrawRectangle(XPens.Black, X(x), Y(y), X(width), Y(height));
+            }
         }
 
-        private void AddText(string text, double x, double y, double width, double height, int halign, int valign, double size)
+        private void AddText(XGraphics gfx, string text, double x, double y, double width, double height, int halign, int valign, double size)
         {
-            Font font = new Font(bf, DY(size), Font.NORMAL, BaseColor.BLACK);
-            var col = new ColumnText(canvas);
+            XPdfFontOptions options = new XPdfFontOptions(PdfFontEncoding.Unicode, PdfFontEmbedding.Always);
+            XFont font = new XFont("Calibri", Y(size), XFontStyle.Regular, options);
 
-            int alignment;
+            XStringFormat format = new XStringFormat();
+            XRect rect = new XRect(X(x), Y(y), X(width), Y(height));
+
             switch (halign)
             {
-                case 0: alignment = Element.ALIGN_LEFT; break;
-                case 1: alignment = Element.ALIGN_CENTER; break;
-                case 2: alignment = Element.ALIGN_RIGHT; break;
-                default: alignment = Element.ALIGN_LEFT; break;
+                case 0: format.Alignment = XStringAlignment.Near; break;
+                case 1: format.Alignment = XStringAlignment.Center; break;
+                case 2: format.Alignment = XStringAlignment.Far; break;
             }
 
-            float leading;
             switch (valign)
             {
-                case 0: leading = -(DY(height) - (font.CalculatedSize)); break;
-                case 1: leading = -((DY(height) / 2f) - (font.CalculatedSize / 3f)); break;
-                case 2: leading = -(font.CalculatedSize / 3f); break;
-                default: leading = -(DY(height) - (font.CalculatedSize)); break;
+                case 0: format.LineAlignment = XLineAlignment.Near; break;
+                case 1: format.LineAlignment = XLineAlignment.Center; break;
+                case 2: format.LineAlignment = XLineAlignment.Far; break;
             }
 
-            col.SetSimpleColumn(X(x), Y(y + height), X(x + width), Y(y + height), leading, alignment);
-            col.AddText(new Phrase(text, font));
-            col.Go();
+            gfx.DrawString(text, font, XBrushes.Black, rect, format);
         }
 
         #endregion
 
         #region Export
 
-        private void Export(BlockItem block)
+        private void Export(XGraphics gfx, BlockItem block)
         {
-            Export(block.Lines);
-            Export(block.Rectangles);
-            Export(block.Ellipses);
-            Export(block.Texts);
-            Export(block.Blocks);
+            Export(gfx, block.Lines);
+            Export(gfx, block.Rectangles);
+            Export(gfx, block.Ellipses);
+            Export(gfx, block.Texts);
+            Export(gfx, block.Blocks);
         }
 
-        private void Export(IEnumerable<BlockItem> blocks)
+        private void Export(XGraphics gfx, IEnumerable<BlockItem> blocks)
         {
             foreach (var block in blocks)
             {
-                Export(block);
+                Export(gfx, block);
             }
         }
 
-        private void Export(IEnumerable<LineItem> lines)
+        private void Export(XGraphics gfx, IEnumerable<LineItem> lines)
         {
             foreach (var line in lines)
             {
-                AddLine(line.X1, line.Y1, line.X2, line.Y2);
+                AddLine(gfx, line.X1, line.Y1, line.X2, line.Y2);
             }
         }
 
-        private void Export(IEnumerable<RectangleItem> rectangles)
+        private void Export(XGraphics gfx, IEnumerable<RectangleItem> rectangles)
         {
             foreach (var rectangle in rectangles)
             {
-                AddRectangle(rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height, rectangle.IsFilled);
+                AddRectangle(gfx, rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height, rectangle.IsFilled);
             }
         }
 
-        private void Export(IEnumerable<EllipseItem> ellipses)
+        private void Export(XGraphics gfx, IEnumerable<EllipseItem> ellipses)
         {
             foreach (var ellipse in ellipses)
             {
-                AddEllipse(ellipse.X, ellipse.Y, ellipse.Width, ellipse.Height, ellipse.IsFilled);
+                AddEllipse(gfx, ellipse.X, ellipse.Y, ellipse.Width, ellipse.Height, ellipse.IsFilled);
             }
         }
 
-        private void Export(IEnumerable<TextItem> texts)
+        private void Export(XGraphics gfx, IEnumerable<TextItem> texts)
         {
             foreach (var text in texts)
             {
-                AddText(text.Text, text.X, text.Y, text.Width, text.Height, text.HAlign, text.VAlign, text.Size);
+                AddText(gfx, text.Text, text.X, text.Y, text.Width, text.Height, text.HAlign, text.VAlign, text.Size);
             }
         }
 
