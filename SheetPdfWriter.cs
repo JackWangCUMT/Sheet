1b7ec48397d7ef5ef2fd9886a542eb28a41b599d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2014-05-01 11:56:40 +0200

    Refactored pdf writer

--- SheetPdfWriter.cs
+++ SheetPdfWriter.cs
@@ -2,13 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
 
 namespace Sheet
 {
-    using PdfSharp;
-    using PdfSharp.Drawing;
-    using PdfSharp.Pdf;
-
     public class SheetPdfWriter
     {
         #region Fields
@@ -25,78 +24,113 @@ namespace Sheet
 
         #endregion
 
-        #region Create
-
-        public void Create(string fileName, double sourceWidth, double sourceHeight, BlockItem block)
-        {
-            PdfDocument document = new PdfDocument();
-            PdfPage page = document.AddPage();
-            page.Size = PageSize.A4;
-            page.Orientation = PageOrientation.Landscape;
-            XGraphics gfx = XGraphics.FromPdfPage(page);
-
-            double scaleX = page.Width.Value / sourceWidth;
-            double scaleY = page.Height.Value / sourceHeight;
-
-            X = (x) => x * scaleX;
-            Y = (y) => y * scaleY;
-
-            Export(gfx, block);
-
-            document.Save(fileName);
-        }
-
-        #endregion
-
-        #region Add
+        #region Properties
 
         /// <summary>
         /// Default XPen stroke thickness in millimeters.
         /// </summary>
-        private double DefaultStrokeThickness = 1.20;
+        public double defaultStrokeThickness = 1.20;
+        public double DefaultStrokeThickness
+        {
+            get { return defaultStrokeThickness; }
+            set
+            {
+                defaultStrokeThickness = value;
+            }
+        }
 
-        private void AddLine(XGraphics gfx, LineItem line)
+        #endregion
+
+        #region Create
+
+        public void Create(string fileName, double sourceWidth, double sourceHeight, BlockItem blockItem)
+        {
+            using (var document = new PdfDocument())
+            {
+                CreatePage(document, sourceWidth, sourceHeight, blockItem);
+                document.Save(fileName);
+            }
+        }
+
+        public void Create(string fileName, double sourceWidth, double sourceHeight, IEnumerable<BlockItem> blockItems)
+        {
+            using (var document = new PdfDocument())
+            {
+                foreach (var blockItem in blockItems)
+                {
+                    CreatePage(document, sourceWidth, sourceHeight, blockItem);
+                }
+
+                document.Save(fileName);
+            }
+        }
+
+        private void CreatePage(PdfDocument document, double sourceWidth, double sourceHeight, BlockItem blockItem)
+        {
+            // create A4 page with landscape orientation
+            PdfPage page = document.AddPage();
+            page.Size = PageSize.A4;
+            page.Orientation = PageOrientation.Landscape;
+
+            using (XGraphics gfx = XGraphics.FromPdfPage(page))
+            {
+                // calculate x and y page scale factors
+                double scaleX = page.Width.Value / sourceWidth;
+                double scaleY = page.Height.Value / sourceHeight;
+                X = (x) => x * scaleX;
+                Y = (y) => y * scaleY;
+
+                // draw block contents to pdf graphics
+                DrawBlock(gfx, blockItem);
+            }
+        }
+
+        #endregion
+
+        #region Draw
+
+        private void DrawLine(XGraphics gfx, LineItem line)
         {
             var pen = new XPen(XColor.FromArgb(line.Stroke.Alpha, line.Stroke.Red, line.Stroke.Green, line.Stroke.Blue),
-                X(line.StrokeThickness == 0.0 ? XUnit.FromMillimeter(DefaultStrokeThickness).Value : line.StrokeThickness)) { LineCap = XLineCap.Round };
+                X(line.StrokeThickness == 0.0 ? XUnit.FromMillimeter(defaultStrokeThickness).Value : line.StrokeThickness)) { LineCap = XLineCap.Round };
             gfx.DrawLine(pen, X(line.X1), Y(line.Y1), X(line.X2), Y(line.Y2));
         }
 
-        private void AddRectangle(XGraphics gfx, RectangleItem rectangle)
+        private void DrawRectangle(XGraphics gfx, RectangleItem rectangle)
         {
             if (rectangle.IsFilled)
             {
                 var pen = new XPen(XColor.FromArgb(rectangle.Stroke.Alpha, rectangle.Stroke.Red, rectangle.Stroke.Green, rectangle.Stroke.Blue),
-                    X(rectangle.StrokeThickness == 0.0 ? XUnit.FromMillimeter(DefaultStrokeThickness).Value : rectangle.StrokeThickness)) { LineCap = XLineCap.Round };
+                    X(rectangle.StrokeThickness == 0.0 ? XUnit.FromMillimeter(defaultStrokeThickness).Value : rectangle.StrokeThickness)) { LineCap = XLineCap.Round };
                 var brush = new XSolidBrush(XColor.FromArgb(rectangle.Fill.Alpha, rectangle.Fill.Red, rectangle.Fill.Green, rectangle.Fill.Blue));
                 gfx.DrawRectangle(pen, brush, X(rectangle.X), Y(rectangle.Y), X(rectangle.Width), Y(rectangle.Height));
             }
             else
             {
                 var pen = new XPen(XColor.FromArgb(rectangle.Stroke.Alpha, rectangle.Stroke.Red, rectangle.Stroke.Green, rectangle.Stroke.Blue),
-                    X(rectangle.StrokeThickness == 0.0 ? XUnit.FromMillimeter(DefaultStrokeThickness).Value : rectangle.StrokeThickness)) { LineCap = XLineCap.Round };
+                    X(rectangle.StrokeThickness == 0.0 ? XUnit.FromMillimeter(defaultStrokeThickness).Value : rectangle.StrokeThickness)) { LineCap = XLineCap.Round };
                 gfx.DrawRectangle(pen, X(rectangle.X), Y(rectangle.Y), X(rectangle.Width), Y(rectangle.Height));
             }
         }
 
-        private void AddEllipse(XGraphics gfx, EllipseItem ellipse)
+        private void DrawEllipse(XGraphics gfx, EllipseItem ellipse)
         {
             if (ellipse.IsFilled)
             {
                 var pen = new XPen(XColor.FromArgb(ellipse.Stroke.Alpha, ellipse.Stroke.Red, ellipse.Stroke.Green, ellipse.Stroke.Blue),
-                    X(ellipse.StrokeThickness == 0.0 ? XUnit.FromMillimeter(DefaultStrokeThickness).Value : ellipse.StrokeThickness)) { LineCap = XLineCap.Round };
+                    X(ellipse.StrokeThickness == 0.0 ? XUnit.FromMillimeter(defaultStrokeThickness).Value : ellipse.StrokeThickness)) { LineCap = XLineCap.Round };
                 var brush = new XSolidBrush(XColor.FromArgb(ellipse.Fill.Alpha, ellipse.Fill.Red, ellipse.Fill.Green, ellipse.Fill.Blue));
                 gfx.DrawEllipse(pen, brush, X(ellipse.X), Y(ellipse.Y), X(ellipse.Width), Y(ellipse.Height));
             }
             else
             {
                 var pen = new XPen(XColor.FromArgb(ellipse.Stroke.Alpha, ellipse.Stroke.Red, ellipse.Stroke.Green, ellipse.Stroke.Blue),
-                    X(ellipse.StrokeThickness == 0.0 ? XUnit.FromMillimeter(DefaultStrokeThickness).Value : ellipse.StrokeThickness)) { LineCap = XLineCap.Round };
+                    X(ellipse.StrokeThickness == 0.0 ? XUnit.FromMillimeter(defaultStrokeThickness).Value : ellipse.StrokeThickness)) { LineCap = XLineCap.Round };
                 gfx.DrawEllipse(pen, X(ellipse.X), Y(ellipse.Y), X(ellipse.Width), Y(ellipse.Height));
             }
         }
 
-        private void AddText(XGraphics gfx, TextItem text)
+        private void DrawText(XGraphics gfx, TextItem text)
         {
             XPdfFontOptions options = new XPdfFontOptions(PdfFontEncoding.Unicode, PdfFontEmbedding.Always);
             XFont font = new XFont("Calibri", Y(text.Size), XFontStyle.Regular, options);
@@ -125,57 +159,53 @@ namespace Sheet
             gfx.DrawString(text.Text, font, brushForeground, rect, format);
         }
 
-        #endregion
-
-        #region Export
-
-        private void Export(XGraphics gfx, BlockItem block)
-        {
-            Export(gfx, block.Lines);
-            Export(gfx, block.Rectangles);
-            Export(gfx, block.Ellipses);
-            Export(gfx, block.Texts);
-            Export(gfx, block.Blocks);
-        }
-
-        private void Export(XGraphics gfx, IEnumerable<BlockItem> blocks)
-        {
-            foreach (var block in blocks)
-            {
-                Export(gfx, block);
-            }
-        }
-
-        private void Export(XGraphics gfx, IEnumerable<LineItem> lines)
+        private void DrawLines(XGraphics gfx, IEnumerable<LineItem> lines)
         {
             foreach (var line in lines)
             {
-                AddLine(gfx, line);
+                DrawLine(gfx, line);
             }
         }
 
-        private void Export(XGraphics gfx, IEnumerable<RectangleItem> rectangles)
+        private void DrawRectangles(XGraphics gfx, IEnumerable<RectangleItem> rectangles)
         {
             foreach (var rectangle in rectangles)
             {
-                AddRectangle(gfx, rectangle);
+                DrawRectangle(gfx, rectangle);
             }
         }
 
-        private void Export(XGraphics gfx, IEnumerable<EllipseItem> ellipses)
+        private void DrawEllipses(XGraphics gfx, IEnumerable<EllipseItem> ellipses)
         {
             foreach (var ellipse in ellipses)
             {
-                AddEllipse(gfx, ellipse);
+                DrawEllipse(gfx, ellipse);
             }
         }
 
-        private void Export(XGraphics gfx, IEnumerable<TextItem> texts)
+        private void DrawTexts(XGraphics gfx, IEnumerable<TextItem> texts)
         {
             foreach (var text in texts)
             {
-                AddText(gfx, text);
+                DrawText(gfx, text);
             }
+        }
+
+        private void DrawBlocks(XGraphics gfx, IEnumerable<BlockItem> blocks)
+        {
+            foreach (var block in blocks)
+            {
+                DrawBlock(gfx, block);
+            }
+        }
+
+        private void DrawBlock(XGraphics gfx, BlockItem block)
+        {
+            DrawLines(gfx, block.Lines);
+            DrawRectangles(gfx, block.Rectangles);
+            DrawEllipses(gfx, block.Ellipses);
+            DrawTexts(gfx, block.Texts);
+            DrawBlocks(gfx, block.Blocks);
         }
 
         #endregion
