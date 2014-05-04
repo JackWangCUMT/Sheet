a5f118a31d7df2ae903e89e0d2e9087ea8a85bb7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2014-05-04 15:47:10 +0200

    Initial dxf library import

---
+++ BlockDxfWriter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dxf;
+using Dxf.Core;
+using Dxf.Enums;
+using Dxf.Classes;
+using Dxf.Tables;
+using Dxf.Blocks;
+using Dxf.Entities;
+using Dxf.Objects;
+
+namespace Sheet
+{
+    public class BlockDxfWriter
+    {
+        // TODO:
+    }
+}
