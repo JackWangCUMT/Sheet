6165381f03081bee12a8535d5a9e8799a947ceaa
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2014-05-17 13:30:39 +0200

    Moved base64 class

--- Sheet.cs
+++ Sheet.cs
@@ -22,6 +22,91 @@ using System.Windows.Shapes;
 
 namespace Sheet
 {
+    #region Base64
+
+    public static class Base64
+    {
+        public static string ToBase64(byte[] bytes)
+        {
+            if (bytes != null)
+            {
+                return Convert.ToBase64String(bytes);
+            }
+            return null;
+        }
+
+        public static MemoryStream ToStream(byte[] bytes)
+        {
+            if (bytes != null)
+            {
+                return new MemoryStream(bytes, 0, bytes.Length);
+            }
+            return null;
+        }
+
+        public static byte[] ToBytes(string base64)
+        {
+            if (!string.IsNullOrEmpty(base64))
+            {
+                return Convert.FromBase64String(base64);
+            }
+            return null;
+        }
+
+        public static MemoryStream ToStream(string base64)
+        {
+            if (!string.IsNullOrEmpty(base64))
+            {
+                byte[] bytes = ToBytes(base64);
+                if (bytes != null)
+                {
+                    return new MemoryStream(bytes, 0, bytes.Length);
+                }
+                return null;
+            }
+            return null;
+        }
+
+        public static byte[] ReadAllBytes(string path)
+        {
+            if (!string.IsNullOrEmpty(path))
+            {
+                return System.IO.File.ReadAllBytes(path);
+            }
+            return null;
+        }
+
+        public static string FromFileToBase64(string path)
+        {
+            if (!string.IsNullOrEmpty(path))
+            {
+                byte[] bytes = ReadAllBytes(path);
+                if (bytes != null)
+                {
+                    return ToBase64(bytes);
+                }
+                return null;
+            }
+            return null;
+        }
+
+        public static MemoryStream FromFileToStream(string path)
+        {
+            if (!string.IsNullOrEmpty(path))
+            {
+                byte[] bytes = ReadAllBytes(path);
+                if (bytes != null)
+                {
+                    return new MemoryStream(bytes, 0, bytes.Length);
+                }
+                return null;
+            }
+            return null;
+        }
+    }
+
+    #endregion
+
     #region Entry Model
 
     public abstract class Entry
@@ -3269,91 +3354,6 @@ namespace Sheet
         }
 
         #endregion
-    }
-
-    #endregion
-
-    #region Base64
-
-    public static class Base64
-    {
-        public static string ToBase64(byte[] bytes)
-        {
-            if (bytes != null)
-            {
-                return Convert.ToBase64String(bytes);
-            }
-            return null;
-        }
-
-        public static MemoryStream ToStream(byte[] bytes)
-        {
-            if (bytes != null)
-            {
-                return new MemoryStream(bytes, 0, bytes.Length); 
-            }
-            return null;
-        }
-
-        public static byte[] ToBytes(string base64)
-        {
-            if (!string.IsNullOrEmpty(base64))
-            {
-                return Convert.FromBase64String(base64);
-            }
-            return null;
-        }
-
-        public static MemoryStream ToStream(string base64)
-        {
-            if (!string.IsNullOrEmpty(base64))
-            {
-                byte[] bytes = ToBytes(base64);
-                if (bytes != null)
-                {
-                    return new MemoryStream(bytes, 0, bytes.Length);
-                }
-                return null;
-            }
-            return null;
-        }
-
-        public static byte[] ReadAllBytes(string path)
-        {
-            if (!string.IsNullOrEmpty(path))
-            {
-                return System.IO.File.ReadAllBytes(path);
-            }
-            return null;
-        }
-
-        public static string FromFileToBase64(string path)
-        {
-            if (!string.IsNullOrEmpty(path))
-            {
-                byte[] bytes = ReadAllBytes(path);
-                if (bytes != null)
-                {
-                    return ToBase64(bytes); 
-                }
-                return null;
-            }
-            return null;
-        }
-
-        public static MemoryStream FromFileToStream(string path)
-        {
-            if (!string.IsNullOrEmpty(path))
-            {
-                byte[] bytes = ReadAllBytes(path);
-                if (bytes != null)
-                {
-                    return new MemoryStream(bytes, 0, bytes.Length); 
-                }
-                return null;
-            }
-            return null;
-        }
     }
 
     #endregion
