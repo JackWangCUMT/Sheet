bdf4d3337769746a9493f48f578467ffc4a6857c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2014-05-20 19:26:11 +0200

    Changed database controller interface

--- Core/Controllers.cs
+++ Core/Controllers.cs
@@ -35,6 +35,9 @@ namespace Sheet
 
     public interface IDatabaseController
     {
+        string Name { get; set; }
+        string[] Columns { get; set; }
+        List<string[]> Data { get; set; }
         string[] Get(int index);
         bool Update(int index, string[] item);
         int Add(string[] item);
