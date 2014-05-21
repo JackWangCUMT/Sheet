7f6a8d44407e2c3a67bf90e9e06e4d111d99e75c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2014-05-21 23:16:10 +0200

    Added wpf FrameworkElementProperties class

--- Core/Wpf.cs
+++ Core/Wpf.cs
@@ -11,6 +11,30 @@ using System.Windows.Shapes;
 
 namespace Sheet
 {
+    #region FrameworkElement Properties
+
+    public static class FrameworkElementProperties
+    {
+        #region IsSelected
+
+        public static bool GetIsSelected(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsSelectedProperty);
+        }
+
+        public static void SetIsSelected(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsSelectedProperty, value);
+        }
+
+        public static readonly DependencyProperty IsSelectedProperty =
+            DependencyProperty.Register("IsSelected", typeof(bool), typeof(FrameworkElement), new PropertyMetadata(false));
+
+        #endregion
+    }
+
+    #endregion
+
     #region WpfVisualHelper
 
     public static class WpfVisualHelper
