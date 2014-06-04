7085ea6c29535eaff41050296a8a00851e8acafd
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2014-06-04 20:26:48 +0200

    Added simulation test class

--- Core/Simulation.cs
+++ Core/Simulation.cs
@@ -232,6 +232,18 @@ namespace Simulation
         public Context() : base() { }
     }
 
+    public class Project : Element
+    {
+        public Project() : base() { }
+    }
+
+    public class Solution : Element
+    {
+        public Solution() : base()  { Tags = new ObservableCollection<Tag>(); }
+        public ObservableCollection<Tag> Tags { get; set; }
+        public Tag DefaultTag { get; set; }
+    }
+
     #endregion
 
     #region Simulation.Core
@@ -2340,6 +2352,226 @@ namespace Simulation
             CurrentSimulationContext.SimulationClock.Cycle = 0;
             CurrentSimulationContext.SimulationClock.Resolution = 0;
         }
+
+        #endregion
+    }
+
+    #endregion
+
+    #region Test
+
+    public class TestSimulation
+    {
+        #region Properties
+
+        private Solution _solution;
+        private Clock _clock;
+        private int _periodInMillisencods;
+        private bool _isSimulationRunning = false;
+
+        #endregion
+
+        #region Properties
+
+        public Solution Solution { get { return _solution; } }
+        public bool IsSimulationRunning { get { return _isSimulationRunning; } }
+
+        #endregion
+
+        #region Constructor
+
+        public TestSimulation(Solution solution, int periodInMillisencods = 100)
+        {
+            _solution = solution;
+            _periodInMillisencods = periodInMillisencods;
+            Initialize();
+        }
+
+        #endregion
+
+        #region Tags
+
+        public Tag CreateSignalTag(string designation, string description, string signal, string condition)
+        {
+            var tag = new Tag()
+            {
+                Id = Guid.NewGuid().ToString()
+            };
+
+            tag.Properties.Add("Designation", new Property(designation));
+            tag.Properties.Add("Description", new Property(description));
+            tag.Properties.Add("Signal", new Property(signal));
+            tag.Properties.Add("Condition", new Property(condition));
+
+            return tag;
+        }
+
+        private void ResetTags()
+        {
+            if (_solution == null || _solution.Tags == null)
+                return;
+
+            // clear Tags children
+            foreach (var tag in _solution.Tags)
+            {
+                foreach (var pin in tag.Children.Cast<Pin>())
+                {
+                    pin.SimulationParent = null;
+                }
+
+                if (tag.Children != null)
+                    tag.Children.Clear();
+            }
+        }
+
+        private void MapSignalToTag(Signal signal, Tag tag)
+        {
+            if (signal == null || signal.Children == null || tag == null || tag.Children == null)
+                return;
+
+            foreach (var pin in signal.Children.Cast<Pin>())
+            {
+                tag.Children.Add(pin);
+
+                // set simulation parent to Tag
+                pin.SimulationParent = tag;
+            }
+        }
+
+        private void MapSignalsToTag(List<Signal> signals)
+        {
+            if (signals == null)
+                return;
+
+            // map each Signal children to Tag
+            foreach (var signal in signals)
+            {
+                var tag = signal.Tag;
+
+                MapSignalToTag(signal, tag);
+            }
+        }
+
+        public void MapTags(List<Context> contexts)
+        {
+            var signals = contexts.SelectMany(x => x.Children).Where(y => y is Signal).Cast<Signal>().ToList();
+
+            ResetTags();
+
+            MapSignalsToTag(signals);
+        }
+
+        private void DebugPrintTagMap()
+        {
+            // print debug
+            foreach (var tag in _solution.Tags)
+            {
+                Debug.Print("{0}", tag.Properties["Designation"].Data);
+
+                foreach (var pin in tag.Children.Cast<Pin>())
+                {
+                    Debug.Print("    -> pin type: {0}, parent: {1}, id: {2}", pin.Type, pin.Parent.Name, pin.Id);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Simulation
+
+        private void Initialize()
+        {
+            _clock = new Clock();
+
+            // create simulation context
+            SimulationFactory.CurrentSimulationContext = new SimulationContext()
+            {
+                Cache = null,
+                SimulationClock = _clock
+            };
+
+            SimulationFactory.IsConsole = false;
+
+            // disable Console debug output
+            SimulationSettings.EnableDebug = false;
+
+            // disable Log for Run()
+            SimulationSettings.EnableLog = false;
+        }
+
+        public void Start()
+        {
+            // simulation is already running
+            if (SimulationFactory.CurrentSimulationContext.SimulationTimer != null)
+                return;
+
+            if (_solution == null)
+                return;
+
+            var projects = _solution.Children.Cast<Project>();
+            if (projects == null)
+                return;
+
+            var contexts = projects.SelectMany(x => x.Children).Cast<Context>().ToList();
+            if (contexts == null)
+                return;
+
+            // map tags
+            MapTags(contexts);
+
+            if (SimulationSettings.EnableDebug)
+            {
+                DebugPrintTagMap();
+            }
+
+            // set elements Id
+            var elements = contexts.SelectMany(x => x.Children).Concat(_solution.Tags);
+            UInt32 elementId = 0;
+
+            foreach (var element in elements)
+            {
+                element.ElementId = elementId;
+                elementId++;
+
+                // set simulation parent
+                if (element.SimulationParent == null)
+                {
+                    element.SimulationParent = element.Parent;
+                }
+            }
+
+            // simulation period in ms
+            int period = _periodInMillisencods;
+
+            // reset previous simulation cache
+            SimulationFactory.Reset(true);
+
+            // run simulation
+            SimulationFactory.Run(contexts, _solution.Tags, period, () => { });
+            _isSimulationRunning = true;
+
+            // reset simulation parent
+            foreach (var element in elements)
+            {
+                element.SimulationParent = null;
+            }
+        }
+
+        public void Stop()
+        {
+            if (SimulationFactory.CurrentSimulationContext.SimulationTimer != null)
+            {
+                // stop simulation
+                SimulationFactory.Stop();
+
+                _isSimulationRunning = false;
+
+                // reset previous simulation cache
+                SimulationFactory.Reset(true);
+
+                ResetTags();
+            }
+        } 
 
         #endregion
     }
