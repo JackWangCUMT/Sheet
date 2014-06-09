9ef40c0acefd27417cf2a812be2f2be04a09c68e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2014-06-09 18:33:04 +0200

    Added block simulation helper interface

---
+++ Simulation/Tests/IBlockSimulationHelper.cs
@@ -0,0 +1,20 @@
+﻿using Sheet.Block.Core;
+using Sheet.Simulation.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sheet.Simulation.Tests
+{
+    public interface IBlockSimulationHelper
+    {
+        void SeState(ILine line, IBoolState state);
+        void SeState(IRectangle rectangle, IBoolState state);
+        void SeState(IEllipse ellipse, IBoolState state);
+        void SeState(IText text, IBoolState state);
+        void SeState(IImage image, IBoolState state);
+        void SeState(IBlock parent, IBoolState state);
+    }
+}
