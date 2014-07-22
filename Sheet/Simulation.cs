156790402a50a8b363585d72cece200657e55558
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2014-07-22 17:42:15 +0200

    Refactored connections class

--- Sheet/Simulation.cs
+++ Sheet/Simulation.cs
@@ -1628,7 +1628,8 @@ namespace Sheet.Simulation
     {
         #region StateSimulationDict
 
-        public Dictionary<Type, Func<Element, ISimulation>> StateSimulationDict = new Dictionary<Type, Func<Element, ISimulation>>()
+        public Dictionary<Type, Func<Element, ISimulation>> StateSimulationDict 
+            = new Dictionary<Type, Func<Element, ISimulation>>()
         {
             // Tag
             {
@@ -1697,6 +1698,150 @@ namespace Sheet.Simulation
                 }
             }
         };
+
+        #endregion
+
+        #region Connections
+
+        private Dictionary<UInt32, List<Tuple<Pin, bool>>> MapPinsToWires(
+            Element[] elements)
+        {
+            int lenght = elements.Length;
+            var dict = new Dictionary<UInt32, List<Tuple<Pin, bool>>>();
+
+            for (int i = 0; i < lenght; i++)
+            {
+                var element = elements[i];
+                if (element is Wire)
+                {
+                    var wire = element as Wire;
+                    var start = wire.Start;
+                    var end = wire.End;
+                    bool inverted = wire.InvertStart | wire.InvertEnd;
+
+                    var startId = start.ElementId;
+                    var endId = end.ElementId;
+
+                    if (!dict.ContainsKey(startId))
+                    {
+                        dict.Add(startId, new List<Tuple<Pin, bool>>());
+                    }
+
+                    dict[startId].Add(new Tuple<Pin, bool>(start, inverted));
+                    dict[startId].Add(new Tuple<Pin, bool>(end, inverted));
+
+                    if (!dict.ContainsKey(endId))
+                    {
+                        dict.Add(endId, new List<Tuple<Pin, bool>>());
+                    }
+
+                    dict[endId].Add(new Tuple<Pin, bool>(start, inverted));
+                    dict[endId].Add(new Tuple<Pin, bool>(end, inverted));
+                }
+            }
+
+            return dict;
+        }
+
+        private void Find(
+            Pin root, 
+            Pin pin, 
+            Dictionary<UInt32, Tuple<Pin, bool>> connections, 
+            Dictionary<UInt32, List<Tuple<Pin, bool>>> pinToWireDict, 
+            int level)
+        {
+            var connectedPins = pinToWireDict[pin.ElementId].Where(x => x.Item1 != pin && x.Item1 != root && connections.ContainsKey(x.Item1.ElementId) == false);
+
+            foreach (var p in connectedPins)
+            {
+                connections.Add(p.Item1.ElementId, p);
+
+                if (SimulationSettings.EnableDebug)
+                {
+                    Debug.Print("{0}    Pin: {1} | Inverted: {2} | SimulationParent: {3} | Type: {4}",
+                        new string(' ', level),
+                        p.Item1.ElementId,
+                        p.Item2,
+                        p.Item1.SimulationParent.ElementId,
+                        p.Item1.Type);
+                }
+
+                if (p.Item1.Type == PinType.Undefined && pinToWireDict.ContainsKey(pin.ElementId) == true)
+                {
+                    Find(root, p.Item1, connections, pinToWireDict, level + 4);
+                }
+            }
+        }
+
+        public void Find(Element[] elements)
+        {
+            if (SimulationSettings.EnableDebug)
+            {
+                Debug.Print("");
+                Debug.Print("--- FindConnections(), elements.Count: {0}", elements.Count());
+                Debug.Print("");
+            }
+
+            var pinToWireDict = MapPinsToWires(elements);
+
+            var pins = elements.Where(x => x is IStateSimulation && x.Children != null)
+                               .SelectMany(x => x.Children)
+                               .Cast<Pin>()
+                               .Where(p => (p.Type == PinType.Undefined || p.Type == PinType.Input) && pinToWireDict.ContainsKey(p.ElementId))
+                               .ToArray();
+
+            var lenght = pins.Length;
+
+            for (int i = 0; i < lenght; i++)
+            {
+                var pin = pins[i];
+
+                if (SimulationSettings.EnableDebug)
+                {
+                    Debug.Print("Pin  {0} | SimulationParent: {1} | Type: {2}",
+                        pin.ElementId,
+                        (pin.SimulationParent != null) ? pin.SimulationParent.ElementId : UInt32.MaxValue,
+                        pin.Type);
+                }
+
+                var connections = new Dictionary<UInt32, Tuple<Pin, bool>>();
+
+                Find(pin, pin, connections, pinToWireDict, 0);
+
+                if (connections.Count > 0)
+                {
+                    pin.Connections = connections.Values.ToArray();
+                }
+                else
+                {
+                    pin.Connections = null;
+                }
+            }
+
+            if (SimulationSettings.EnableDebug)
+            {
+                Debug.Print("");
+            }
+
+            pinToWireDict = null;
+            pins = null;
+        }
+
+        public void Reset(IEnumerable<Pin> pins)
+        {
+            foreach (var pin in pins)
+            {
+                if (pin.IsPinTypeUndefined)
+                {
+                    pin.Connections = null;
+                    pin.Type = PinType.Undefined;
+                }
+                else
+                {
+                    pin.Connections = null;
+                }
+            }
+        }
 
         #endregion
 
@@ -1818,7 +1963,9 @@ namespace Sheet.Simulation
             }
         }
 
-        private void GenerateCompileCache(List<ISimulation> simulations, IClock clock)
+        private void GenerateCompileCache(
+            List<ISimulation> simulations, 
+            IClock clock)
         {
             if (simulations == null)
             {
@@ -1852,17 +1999,18 @@ namespace Sheet.Simulation
             }
         }
 
-        private SimulationCache Compile(Element[] elements, IClock clock)
+        private SimulationCache Compile(
+            Element[] elements, 
+            IClock clock)
         {
             var sw = System.Diagnostics.Stopwatch.StartNew();
 
             var cache = new SimulationCache();
-            var connections = new Connections();
 
             // -- step 1: reset pin connections ---
             var pins = elements.Where(x => x is Pin).Cast<Pin>();
 
-            connections.Reset(pins);
+            Reset(pins);
 
             // -- step 2: initialize IStateSimulation simulation
             var simulations = new List<ISimulation>();
@@ -1879,7 +2027,7 @@ namespace Sheet.Simulation
             }
 
             // -- step 3: update pin connections ---
-            connections.Find(elements);
+            Find(elements);
 
             if (SimulationSettings.EnableDebug)
             {
@@ -1952,7 +2100,10 @@ namespace Sheet.Simulation
             return cache;
         }
 
-        public SimulationCache Compile(IEnumerable<Context> contexts, IEnumerable<Tag> tags, IClock clock)
+        public SimulationCache Compile(
+            IEnumerable<Context> contexts, 
+            IEnumerable<Tag> tags, 
+            IClock clock)
         {
             var elements = contexts.SelectMany(x => x.Children).Concat(tags).ToArray();
 
@@ -1965,7 +2116,10 @@ namespace Sheet.Simulation
             return cache;
         }
 
-        public SimulationCache Compile(Context context, IEnumerable<Tag> tags, IClock clock)
+        public SimulationCache Compile(
+            Context context, 
+            IEnumerable<Tag> tags, 
+            IClock clock)
         {
             var elements = context.Children.Concat(tags).ToArray();
 
@@ -1976,151 +2130,6 @@ namespace Sheet.Simulation
             System.GC.Collect();
 
             return cache;
-        }
-
-        #endregion
-    }
-
-    public class Connections
-    {
-        #region Find Connections by Id
-
-        private Dictionary<UInt32, List<Tuple<Pin, bool>>> MapPinsToWires(Element[] elements)
-        {
-            int lenght = elements.Length;
-            var dict = new Dictionary<UInt32, List<Tuple<Pin, bool>>>();
-
-            for (int i = 0; i < lenght; i++)
-            {
-                var element = elements[i];
-                if (element is Wire)
-                {
-                    var wire = element as Wire;
-                    var start = wire.Start;
-                    var end = wire.End;
-                    bool inverted = wire.InvertStart | wire.InvertEnd;
-
-                    var startId = start.ElementId;
-                    var endId = end.ElementId;
-
-                    if (!dict.ContainsKey(startId))
-                    {
-                        dict.Add(startId, new List<Tuple<Pin, bool>>());
-                    }
-
-                    dict[startId].Add(new Tuple<Pin, bool>(start, inverted));
-                    dict[startId].Add(new Tuple<Pin, bool>(end, inverted));
-
-                    if (!dict.ContainsKey(endId))
-                    {
-                        dict.Add(endId, new List<Tuple<Pin, bool>>());
-                    }
-
-                    dict[endId].Add(new Tuple<Pin, bool>(start, inverted));
-                    dict[endId].Add(new Tuple<Pin, bool>(end, inverted));
-                }
-            }
-
-            return dict;
-        }
-
-        private void Find(Pin root, Pin pin, Dictionary<UInt32, Tuple<Pin, bool>> connections, Dictionary<UInt32, List<Tuple<Pin, bool>>> pinToWireDict, int level)
-        {
-            var connectedPins = pinToWireDict[pin.ElementId].Where(x => x.Item1 != pin && x.Item1 != root && connections.ContainsKey(x.Item1.ElementId) == false);
-
-            foreach (var p in connectedPins)
-            {
-                connections.Add(p.Item1.ElementId, p);
-
-                if (SimulationSettings.EnableDebug)
-                {
-                    Debug.Print("{0}    Pin: {1} | Inverted: {2} | SimulationParent: {3} | Type: {4}",
-                        new string(' ', level),
-                        p.Item1.ElementId,
-                        p.Item2,
-                        p.Item1.SimulationParent.ElementId,
-                        p.Item1.Type);
-                }
-
-                if (p.Item1.Type == PinType.Undefined && pinToWireDict.ContainsKey(pin.ElementId) == true)
-                {
-                    Find(root, p.Item1, connections, pinToWireDict, level + 4);
-                }
-            }
-        }
-
-        public void Find(Element[] elements)
-        {
-            if (SimulationSettings.EnableDebug)
-            {
-                Debug.Print("");
-                Debug.Print("--- FindConnections(), elements.Count: {0}", elements.Count());
-                Debug.Print("");
-            }
-
-            var pinToWireDict = MapPinsToWires(elements);
-
-            var pins = elements.Where(x => x is IStateSimulation && x.Children != null)
-                               .SelectMany(x => x.Children)
-                               .Cast<Pin>()
-                               .Where(p => (p.Type == PinType.Undefined || p.Type == PinType.Input) && pinToWireDict.ContainsKey(p.ElementId))
-                               .ToArray();
-
-            var lenght = pins.Length;
-
-            for (int i = 0; i < lenght; i++)
-            {
-                var pin = pins[i];
-
-                if (SimulationSettings.EnableDebug)
-                {
-                    Debug.Print("Pin  {0} | SimulationParent: {1} | Type: {2}",
-                        pin.ElementId,
-                        (pin.SimulationParent != null) ? pin.SimulationParent.ElementId : UInt32.MaxValue,
-                        pin.Type);
-                }
-
-                var connections = new Dictionary<UInt32, Tuple<Pin, bool>>();
-
-                Find(pin, pin, connections, pinToWireDict, 0);
-
-                if (connections.Count > 0)
-                {
-                    pin.Connections = connections.Values.ToArray();
-                }
-                else
-                {
-                    pin.Connections = null;
-                }
-            }
-
-            if (SimulationSettings.EnableDebug)
-            {
-                Debug.Print("");
-            }
-
-            pinToWireDict = null;
-            pins = null;
-        }
-
-        #endregion
-
-        #region Reset Connections
-
-        public void Reset(IEnumerable<Pin> pins)
-        {
-            foreach (var pin in pins)
-            {
-                if (pin.IsPinTypeUndefined)
-                {
-                    pin.Connections = null;
-                    pin.Type = PinType.Undefined;
-                }
-                else
-                {
-                    pin.Connections = null;
-                }
-            }
         }
 
         #endregion
